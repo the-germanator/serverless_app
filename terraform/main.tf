006c3cef4d6b76639c7c92a66530cd94d31c7872
Christoph Schild <christoph@schild.tech>
2022-08-06 23:35:36 -0400

    Added GET lambda & test script

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,6 +77,32 @@ resource "aws_lambda_function" "delete_lambda" {
   runtime       = "nodejs16.x"
   source_code_hash = filebase64sha256(data.archive_file.delete_lambda_zip.output_path)
 }
+// GET
+resource "aws_iam_role" "lambda_get_role" {
+  name = "lambda_get_role"
+  assume_role_policy = file("policies/assume_role_policy.json")
+}
+
+resource "aws_iam_role_policy" "get_lambda_policy" {
+  name = "lambda_policy"
+  role = aws_iam_role.lambda_get_role.id
+  policy = file("policies/dynamodb_get.json")
+}
+
+data "archive_file" "get_lambda_zip" {
+    type        = "zip"
+    source_dir  = "../lambdas/get"
+    output_path = "../lambdas/get/get_lambda.zip"
+}
+
+resource "aws_lambda_function" "get_lambda" {
+  function_name = "get-data"
+  filename      = data.archive_file.get_lambda_zip.output_path
+  role          = aws_iam_role.lambda_get_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs16.x"
+  source_code_hash = filebase64sha256(data.archive_file.get_lambda_zip.output_path)
+}
 
 // API Gateway
 
@@ -102,6 +128,14 @@ resource "aws_api_gateway_method" "delete_proxy_root" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_method" "get_proxy_root" {
+  rest_api_id = aws_api_gateway_rest_api.dynamo_data_routes.id
+  resource_id = aws_api_gateway_rest_api.dynamo_data_routes.root_resource_id
+  http_method = "GET"
+  authorization = "NONE"
+  request_parameters = {"method.request.querystring.userID" = true}
+}
+
 
 resource "aws_api_gateway_integration" "put_lambda_root" {
   rest_api_id = aws_api_gateway_rest_api.dynamo_data_routes.id
@@ -120,6 +154,14 @@ resource "aws_api_gateway_integration" "delete_lambda_root" {
   uri = aws_lambda_function.delete_lambda.invoke_arn
 }
 
+resource "aws_api_gateway_integration" "get_lambda_root" {
+  rest_api_id = aws_api_gateway_rest_api.dynamo_data_routes.id
+  resource_id = aws_api_gateway_method.get_proxy_root.resource_id
+  http_method = aws_api_gateway_method.get_proxy_root.http_method
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.get_lambda.invoke_arn
+}
 
 
 resource "aws_api_gateway_deployment" "put-deployment" {
@@ -138,7 +180,13 @@ resource "aws_api_gateway_deployment" "delete-deployment" {
   stage_name = "test"
 }
 
-
+resource "aws_api_gateway_deployment" "get-deployment" {
+  depends_on = [
+    aws_api_gateway_integration.get_lambda_root
+  ]
+  rest_api_id = aws_api_gateway_rest_api.dynamo_data_routes.id
+  stage_name = "test"
+}
 
 // Permissions
 resource "aws_lambda_permission" "put_apigw" {
@@ -153,6 +201,14 @@ resource "aws_lambda_permission" "delete_apigw" {
   statement_id = "AllowAPIGatewayInvoke"
   action = "lambda:InvokeFunction"
   function_name = aws_lambda_function.delete_lambda.function_name
+  principal = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.dynamo_data_routes.execution_arn}/*/*"
+}
+
+resource "aws_lambda_permission" "get_apigw" {
+  statement_id = "AllowAPIGatewayInvoke"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.get_lambda.function_name
   principal = "apigateway.amazonaws.com"
   source_arn = "${aws_api_gateway_rest_api.dynamo_data_routes.execution_arn}/*/*"
 }
